2dc81bf58614548dbfc37e0c5ca26beba078515e
Marine Kitagawa <195569727+kitty4O4@users.noreply.github.com>
2026-05-11 23:58:59 +0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -250,10 +250,10 @@ PB_TORCH_PATH := "/sys/devices/virtual/camera/flash/rear_flash"
 PB_TORCH_MAX_BRIGHTNESS := 1
 
 # Version/Maintainer
-TW_DEVICE_VERSION := SavedByLight & ravindu644
-SHRP_MAINTAINER := SavedByLight & ravindu644
-OF_MAINTAINER := SavedByLight & ravindu644
-MAINTAINER := SavedByLight & ravindu644
+TW_DEVICE_VERSION := SavedByLight, Ravindu644 & Nightwings
+SHRP_MAINTAINER := SavedByLight, Ravindu644 & Nightwings
+OF_MAINTAINER := SavedByLight, Ravindu644 & Nightwings
+MAINTAINER := SavedByLight, Ravindu644 & Nightwings
 
 # For testing only
 BETA_BUILD := true
