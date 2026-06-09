bc0eb28ce10d22d9381571f3598e0fb83e33a538
Marine Kitagawa <195569727+kitty4O4@users.noreply.github.com>
2026-06-09 08:34:29 +0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,7 +61,7 @@ BOARD_KERNEL_CMDLINE :=  \
 	video=vfb:640x400,bpp=32,memsize=3072000 \
 	printk.devkmsg=on \
 	firmware_class.path=/vendor/firmware,/vendor/firmware_mnt/image,/efs/wifi \
-	onsole=null \
+	console=null \
 	bootconfig \
 	androidboot.hardware=qcom \
 	hardware=qcom \
