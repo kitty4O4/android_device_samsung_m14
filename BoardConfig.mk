2a633128c2fddd30e5ae29606a400faf5cb801b0
Marine Kitagawa <195569727+kitty4O4@users.noreply.github.com>
2026-06-09 06:54:30 +0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,7 +72,7 @@ BOARD_KERNEL_CMDLINE :=  \
 	loop.max_part=7
 
 # Kernel
-BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_BOOTIMG_HEADER_VERSION := 4
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 firmware_class.path=/vendor/firmware,/vendor/firmware_mnt/image,/efs/wifi printk.devkmsg=on bootconfig androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=4e00000.dwc3 loop.max_part=7
 BOARD_KERNEL_PAGESIZE := 4096
