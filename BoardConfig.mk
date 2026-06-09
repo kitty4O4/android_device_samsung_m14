ea55c4a58a33b433a55dcfb2ad08339d0c42ea79
Marine Kitagawa <195569727+kitty4O4@users.noreply.github.com>
2026-06-09 22:34:11 +0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,25 +56,10 @@ TW_CUSTOM_CPU_POS := "275"
 TW_CUSTOM_CLOCK_POS := "70"
 TW_CUSTOM_BATTERY_POS := "790"
 
-# Cmdline
-BOARD_KERNEL_CMDLINE :=  \
-	video=vfb:640x400,bpp=32,memsize=3072000 \
-	printk.devkmsg=on \
-	firmware_class.path=/vendor/firmware,/vendor/firmware_mnt/image,/efs/wifi \
-	console=null \
-	bootconfig \
-	androidboot.hardware=qcom \
-	hardware=qcom \
-	androidboot.memcg=1 \
-	androidboot.usbcontroller=a600000.dwc3 \
-	androidboot.init_fatal_panic=true \
-	androidboot.selinux=permissive \
-	loop.max_part=7
-
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 4
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 firmware_class.path=/vendor/firmware,/vendor/firmware_mnt/image,/efs/wifi printk.devkmsg=on bootconfig androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=4e00000.dwc3 loop.max_part=7
+BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 firmware_class.path=/vendor/firmware,/vendor/firmware_mnt/image,/efs/wifi printk.devkmsg=on bootconfig androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=4e00000.dwc3 loop.max_part=7 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x02000000
 BOARD_KERNEL_TAGS_OFFSET := 0x01e00000
